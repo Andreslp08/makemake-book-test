269ace5bd5e83b6acf5f1ad02d1ac138763b1c64
Andrés López <andreslp08@hotmail.com>
2022-02-16 05:11:37 -0500

    pagina 4 agregada

---
+++ src/pages/page-4.tsx
@@ -0,0 +1,60 @@
+import React, { useEffect } from "react";
+import Static from "../assets/images/page 4/static.png";
+import Cloud1 from "../assets/images/page 4/cloud-1.png";
+import Cloud2 from "../assets/images/page 4/cloud-2.png";
+import Cloud3 from "../assets/images/page 4/cloud-3.png";
+import gsap from "gsap";
+
+export const Page4: React.FC = () => {
+
+    useEffect(()=>{
+        const animations = ()=>{
+            const tl = gsap.timeline();
+            tl.fromTo('.clouds-group .cloud', {rotate:'10deg', duration:2}, {rotate:'0deg', yoyo:true, repeat:-1, duration:2 },0);
+            tl.fromTo('.clouds-group', {x:'80vw', duration:30}, {x:'-100vw', repeat:-1,  duration:30 },0);
+        }
+        animations();
+    },[])
+
+	return (
+		<div className="static-background relative" style={{ backgroundImage: `url(${Static})` }}>
+			<div
+				className="cloud absolute"
+				style={{
+					top: "35vh",
+					left: "10vw",
+					width: "70px",
+					height: "70px",
+					backgroundImage: `url(${Cloud3})`,
+				}}
+			></div>
+
+			<div className="clouds-group absolute" style={{top:'15vh',left:'10vw'}}>
+				<div
+					className="cloud"
+					style={{
+						width: "100px",
+						height: "100px",
+						backgroundImage: `url(${Cloud2})`,
+					}}
+				></div>
+				<div
+					className="cloud"
+					style={{
+						width: "100px",
+						height: "100px",
+						backgroundImage: `url(${Cloud1})`,
+					}}
+				></div>
+				<div
+					className="cloud"
+					style={{
+						width: "100px",
+						height: "100px",
+						backgroundImage: `url(${Cloud3})`,
+					}}
+				></div>
+			</div>
+		</div>
+	);
+};
